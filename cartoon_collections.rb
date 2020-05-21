2f0262c964baebe4f069317220826547054ef7a0
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-21 22:53:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ end
 def list_dwarves(array)
   num_list 
   count = 0 
-  while array.each do |dwarves|
-    p array[count].to_s + " #{dwarves}"
+  while array.each do |dwarf|
+    p array[count].to_s + " #{dwarf}"
     count += 1 
   end 
   # Use `each_with_index` to enumerate over the provided array
