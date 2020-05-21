2f4f6886d236f7d70b028974c8c7cffa462beb76
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-21 23:06:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,17 +7,21 @@ def greet_characters(array)
   # Print a custom greeting for each element
 end
 
+# Calls block with two arguments, the item and its index, for each item in enum. Given arguments are passed through to each().
 
 hash = Hash.new
 %w(cat dog wombat).each_with_index { |item, index|
   hash[item] = index
   
 def list_dwarves(array)
-  list = []
-  array.each_with_index do |dwarf, index|
-    list
-    p list.to_s + " #{dwarf}"
-    list += 1
+  index = 0 
+  while index < array.length do 
+    num = 1
+    array.each_with_index do |dwarf, num|
+    p "#{num.to_s}" + ". #{dwarf}"
+    num += 1 
+  end 
+  index += 1 
   end 
   # Use `each_with_index` to enumerate over the provided array
   #
